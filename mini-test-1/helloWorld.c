313f13c810fab38a8d9764b499545f46ac8c6b54
hoonman <jh020812@gmail.com>
2025-10-21 07:47:28 -0700

    hello world practice

--- mini-test-1/helloWorld.c
+++ mini-test-1/helloWorld.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+struct foo5 {
+    char c;
+    struct foo5_inner {
+        char *p;
+        short x;
+    } inner;
+};
+
 int main(void) {
-    
+    int i[3] = {12,4,1}, j = 3, *p = i, *q = &j, *r;
+    int A = 5**p / *q+7;
+    printf("%d \n", *q+7);
+    printf("A %d:\n", A);
+    *(r=&j) += **&p;
+    printf("r %d:\n", *r);
+    printf("Size of foo5: %zu\n", sizeof(struct foo5));
+    return 0;
 }
