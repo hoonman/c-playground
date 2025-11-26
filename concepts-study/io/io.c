5395cef5cedf523ee7645dc4cacb98ea69170282
hoonman <jh020812@gmail.com>
2025-11-26 07:13:35 -0800

    io playground

---
+++ concepts-study/io/io.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+
+// the goal of this script is to understand how IO redirection works.
+// we need to fork the process, use open(), and then dup2 to redirect.
+
+int main() {
+    
+}
